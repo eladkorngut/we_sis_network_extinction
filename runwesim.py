03685ccdd6b000a8e42e870cf10ec6b9d466330f
eladkorngut <elad.korngut@mail.huji.ac.il>
2023-01-16 21:26:08 +0200

    Bug fix, the for loop had a non int version for the range of N

--- runwesim.py
+++ runwesim.py
@@ -48,7 +48,7 @@ def job_to_cluster(foldername,parameters,Istar):
             k * (1 + eps_dout))
         Beta = Beta_avg / (1 + eps_din * eps_dout)  # This is so networks with different std will have the reproduction number
     dir_path = os.path.dirname(os.path.realpath(__file__))
-    for i in range(number_of_networks):
+    for i in range(int(number_of_networks)):
         if prog=='bd':
             G = rand_networks.random_bimodal_directed_graph(int(d1_in), int(d1_out), int(d2_in), int(d2_out), int(N))
         else:
@@ -82,7 +82,7 @@ if __name__ == '__main__':
     jump = 5
     Alpha = 1.0 # Recovery rate
     Beta_avg = Alpha * lam / k # Infection rate for each node
-    eps_din,eps_dout = 0.4,0.0 # The normalized std (second moment divided by the first) of the network
+    eps_din,eps_dout = 0.1,0.0 # The normalized std (second moment divided by the first) of the network
     # G = nx.random_regular_graph(k,N) # Creates a random graphs with k number of neighbors
     relaxation_time  = 10
     # tau = 1/(Num_inf*Alpha+N*Beta*k)
@@ -92,7 +92,7 @@ if __name__ == '__main__':
 
     parameters = np.array([N,sims,it,k,x,lam,jump,Num_inf,number_of_networks,tau,eps_din,eps_dout,new_trajcetory_bin,prog,Beta_avg])
     graphname  = 'GNull'
-    foldername = 'gauss_N400_lam14_tau1_it100_jump5_quant5_sims2000_net10_epsin01_epsout02'
+    foldername = 'gauss_N400_lam14_tau1_it100_jump5_quant5_sims2000_net10_epsin01_epsout0'
     # y1star=(-2*eps_din*(1 + eps_dout*eps_din)+ lam*(-1 + eps_din)*(1 + (-1 + 2*eps_dout)*eps_din)+ np.sqrt(lam**2 +eps_din*(4*eps_din +lam**2*eps_din*(-2 +eps_din**2) +4*eps_dout*(lam -(-2 + lam)*eps_din**2) +4*eps_dout**2*eps_din*(lam -(-1 + lam)*eps_din**2))))/(4*lam*(-1 +eps_dout)*(-1 +eps_din)*eps_din)
     # y2star=(lam + eps_din*(-2 + 2*lam +lam*eps_din+ 2*eps_dout*(lam +(-1 + lam)*eps_din)) -np.sqrt(lam**2 +eps_din*(4*eps_din +lam**2*eps_din*(-2 +eps_din**2) +4*eps_dout*(lam -(-2 + lam)*eps_din**2) +4*eps_dout**2*eps_din*(lam -(-1 + lam)*eps_din**2))))/(4*lam*(1 +eps_dout)*eps_din*(1 + eps_din))
     # Istar = (y1star +y2star)*N
